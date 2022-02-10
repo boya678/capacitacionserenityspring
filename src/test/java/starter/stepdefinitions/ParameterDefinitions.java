9a2a863c7dd30a0608b882964b7ada6729b02a93
Your Name <you@example.com>
2022-02-10 16:42:31 -0500

    test

---
+++ src/test/java/starter/stepdefinitions/ParameterDefinitions.java
@@ -0,0 +1,24 @@
+package starter.stepdefinitions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cucumber.java.Before;
+import io.cucumber.java.ParameterType;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+import org.openqa.selenium.WebDriver;
+
+public class ParameterDefinitions {
+
+    @ParameterType(".*")
+    public Actor actor(String actorName) {
+        return OnStage.theActorCalled(actorName);
+    }
+
+    @Before
+    public void setTheStage() {
+        OnStage.setTheStage(new OnlineCast());
+        WebDriverManager.chromedriver().setup();
+    }
+}
